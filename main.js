dbeaa9743a722d7b1fc6afc49c8e1fa91634d26b
LindseyJ <lindsey.wilson1@outlook.com>
2023-02-18 19:02:23 +0000

    Add mobile navigation, including background overlay

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
+const hbIcon = document.querySelector("#hb-icon");
+const hbIconClose = document.querySelector("#hb-icon-close");
+const overlayElem = document.querySelector("#overlay");
+hbIcon.addEventListener("click", (e) => {
+    //TODO: code what happens when mob menu btn is clicked --OPEN MENU--
+    console.log("menu btn pressed");
+    hbIcon.classList.replace("show", "hide");
+    hbIconClose.classList.replace("hide", "show");
+    overlayElem.classList.add("overlay");
+});
 
+hbIconClose.addEventListener("click", (e) => {
+    //TODO: code what happens when x btn is clicked --CLOSE MENU--
+    console.log("close menu btn pressed");
+    hbIconClose.classList.replace("show", "hide");
+    hbIcon.classList.replace("hide", "show");
+    overlayElem.classList.remove("overlay");
+});
